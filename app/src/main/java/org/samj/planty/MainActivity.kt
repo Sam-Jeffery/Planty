6d6ddb3256127fe5dadebf3789be2db9489f8286
Samuel Jeffery <47988070+Sam-Jeffery@users.noreply.github.com>
2025-02-11 23:40:16 +0000

    Improved plant card design

--- app/src/main/java/org/samj/planty/MainActivity.kt
+++ app/src/main/java/org/samj/planty/MainActivity.kt
@@ -6,25 +6,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,32 +38,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             PlantyTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(
-                        msg = Message("Sam", "Jetpack compose fucking sucks")
-                    )
+                    Column(modifier = Modifier.statusBarsPadding()) {
+                        PlantCard(
+                            plant = Plant("Bonsai", 5)
+                        )
+                    }
                 }
             }
-        }
-    }
-}
-
-data class Message(val author: String, val body: String)
-
-@Composable
-fun MessageCard(msg: Message){
-    Column {
-        Text(
-            text = msg.author,
-            color = MaterialTheme.colorScheme.secondary,
-            style = MaterialTheme.typography.titleSmall
-        )
-        Spacer(modifier = Modifier.height(4.dp))
-        Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp){
-            Text(
-                text = msg.body,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.bodyMedium
-            )
         }
     }
 }
@@ -71,46 +53,56 @@ data class Plant(val name: String, val nextWater: Int)
 
 @Composable
 fun PlantCard(plant: Plant){
-    Column{
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Image(
-                painter = painterResource(R.drawable.ic_launcher_foreground),
-                contentDescription = "Plant icon",
-                modifier = Modifier.size(40.dp).clip(CircleShape)
-            )
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(8.dp)
+    ){
+        Column{
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_foreground),
+                    contentDescription = "Plant icon",
+                    modifier = Modifier.size(40.dp).clip(CircleShape)
+                )
 
-            Spacer(
-                modifier = Modifier.width(8.dp)
-            )
+                Spacer(
+                    modifier = Modifier.width(8.dp)
+                )
 
-            Text(
-                text = plant.name,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.labelMedium
-            )
+                Text(
+                    text = plant.name,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.labelMedium
+                )
 
-            Spacer(
-                modifier = Modifier.weight(1f)
-            )
+                Spacer(
+                    modifier = Modifier.weight(1f)
+                )
 
-            Text(
-                text = "2d",
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.bodyMedium
-            )
+                Text(
+                    text = "2d",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.bodyMedium
+                )
 
-            Spacer(
-                modifier = Modifier.width(8.dp)
+                Spacer(
+                    modifier = Modifier.width(8.dp)
+                )
+            }
+
+            LinearProgressIndicator(
+                progress = { 0.5f },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                color = MaterialTheme.colorScheme.onPrimary,
+                trackColor = MaterialTheme.colorScheme.onPrimaryContainer
             )
         }
-
-        LinearProgressIndicator(
-            progress = { 0.5f },
-            modifier = Modifier.fillMaxWidth(),
-        )
     }
 }
 
